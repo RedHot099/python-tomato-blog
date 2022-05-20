8584d32c324ecbd75eaa6182171d14437e27eeb5
Jakub <jakeic@st.amu.edu.pl>
2022-05-20 21:55:04 +0200

    heroku ready

--- blog/settings.py
+++ blog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*bs1eu+6bfxvrj*jp5)99x5%$ph=cr@)ary()cx99_+ov(6#bo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-tomato-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
